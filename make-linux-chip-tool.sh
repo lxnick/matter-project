c0518d93c8dd8ffa73f1cdf1eeeab709afd6a680
Nick Lin <lxnick@gmail.com>
2023-02-09 01:00:33 +0800

    Update make-linux-chip-tool.sh
    Add Prerequisites for linux

--- make-linux-chip-tool.sh
+++ make-linux-chip-tool.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
 
-# 
+# Reference Link
+# https://github.com/project-chip/connectedhomeip/blob/master/docs/guides/BUILDING.md
+
+# Installing prerequisites on Linux 
+sudo apt-get install git gcc g++ pkg-config libssl-dev libdbus-1-dev \
+     libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev \
+     python3-pip unzip libgirepository1.0-dev libcairo2-dev libreadline-dev
+
+# Download project-chip     
+git clone --recurse-submodules git@github.com:project-chip/connectedhomeip.git     
