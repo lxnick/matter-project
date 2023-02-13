e84c0d1747e5e05a13801e97aed805be5c108fa8
Nick Lin <lxnick@gmail.com>
2023-02-14 01:49:17 +0800

    Add setup-chip-tool-linux.sh

---
+++ setup-chip-tool-linux.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+sudo apt-get install git gcc g++ pkg-config libssl-dev libdbus-1-dev \
+     libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev \
+     python3-pip unzip libgirepository1.0-dev libcairo2-dev libreadline-dev
+
