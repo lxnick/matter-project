053286044973363564e91c044e571b9548622c76
Nick Lin <lxnick@gmail.com>
2023-02-14 00:26:17 +0800

    add setup-chip-tool-python.sh

---
+++ setup-chip-tool-python.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# requirement
+$ sudo apt-get install git gcc g++ python pkg-config libssl-dev libdbus-1-dev libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev python3-pip unzip libgirepository1.0-dev libcairo2-dev bluez
+
+# install
+$ sudo apt-get install  pi-bluetooth
