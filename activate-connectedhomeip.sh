7a12db280f993ea0fdada6c3d3260b8ce8eac556
Nick Lin <lxnick@gmail.com>
2023-02-13 10:24:09 +0800

    add active/build script

---
+++ activate-connectedhomeip.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+echo "Manually input command ....."
+
+# normal command 
+# source scripts/activate.sh
+
+# if environment is out of date
+# source scripts/bootstrap.sh
+
+
---
+++ build-connectedhomeip.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+echo "Activate before build"
+
+# normal command 
+# gn gen out/host
+# ninja -C out/host
+
+
+
